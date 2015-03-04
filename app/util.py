5417d29a378ff710ee1e70435d7213182f5670a6
friendly-flame <armsnyder@gmail.com>
2015-03-04 05:14:17 -0600

    Added docstrings to util Added relative_path function to util

--- app/util.py
+++ app/util.py
@@ -2,6 +2,7 @@
 # Constant values and functions are defined for easy reference
 
 import sys
+import os
 
 
 app_name = 'Cheese Whiz'
@@ -9,24 +10,42 @@ verbose = True
 
 
 def warning(text, exit=False, status=1):
-    """prints a custom error message to console"""
+    """
+    prints a custom error message to console
+    :param text: text to print
+    :param exit: if True, system throws FATAL ERROR and exits
+    :param status: status code to return on system exit
+    """
     if exit:
         sys.stderr.write("FATAL ERROR: "+text+"\n")
         sys.exit(status)
     else:
         sys.stderr.write("WARNING: "+text+"\n")
-    return
 
 
 def close(message=None):
-    """for debugging, exits program and outputs some variable"""
+    """
+    for debugging, exits program and outputs some variable
+    :param message: text to print on exit
+    """
     if message:
         print(message)
     sys.exit(0)
 
 
 def vprint(text):
-    """prints a custom message to console if in verbose mode"""
+    """
+    prints a custom message to console if in verbose mode
+    :param text: text to print
+    """
     if verbose:
         print(text)
-    return
+
+
+def relative_path(path):
+    """
+    Get file path relative to calling script's directory
+    :param path: filename or file path
+    :return: full path name, relative to script location
+    """
+    return os.path.join(os.path.join(os.getcwd(), os.path.dirname(__file__)), path)
