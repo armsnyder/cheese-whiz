d9a5572089447d1473d2c4d50701ee95d308cc8b
nakfoury <nakfoury@gmail.com>
2015-03-12 21:29:48 -0500

    Moved get_html to parser.py. Added parse_html to close #13.

--- app/parser.py
+++ app/parser.py
@@ -1,10 +1,29 @@
+from bs4 import BeautifulSoup
+import urllib2
+
+import util
+
+
 def parse_html(html):
     """
     Takes HTML and does some basic parsing
     :param html: html text
-    :return: ???
+    :return: Title string, list of ingredient/quantity tuples, and list of step strings
     """
-    pass
+    soup = BeautifulSoup(open('C:\\Users\\Neal\\Documents\\Coursework\\EECS\\337\\lasagne_html.html'))
+    title = soup.find('span', {'itemprop': 'name'}).get_text()
+    ingredients = soup.find_all('p', {'itemprop': 'ingredients'})
+    tupes = []
+    for i in ingredients:
+        a = i.find('span', {'class': 'ingredient-amount'}).get_text()
+        n = i.find('span', {'class': 'ingredient-name'}).get_text()
+        tupes.append((n, a))
+
+    directions = soup.find_all('span', {'class': 'plaincharacterwrap break'})
+    steps = []
+    for d in directions:
+        steps.append(d.get_text())
+    return title, tupes, steps
 
 
 def url_to_dictionary(url):
@@ -14,7 +33,23 @@ def url_to_dictionary(url):
     :param url: url of requested recipe
     :return: dictionary in autograder-acceptable format
     """
-    from app.app import get_html
     html = get_html(url)
     # etc
     pass
+
+
+def get_html(url):
+    """
+    Retrieves html text from a string-formatted url
+    :param url: web url string
+    :return: html text
+    """
+    if '://' not in url:
+        formatted_url = 'http://' + url
+    else:
+        formatted_url = url
+    try:
+        return urllib2.urlopen(formatted_url).read()
+    except urllib2.URLError:
+        util.warning('Invalid URL request')
+        return None
